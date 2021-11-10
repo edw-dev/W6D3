9277761b9144461301cbdaa2f8cf0a625f8673e0
anna <annayth@gmail.com>
2021-11-10 15:48:44 -0500

    break

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+    def index
+        # render plain: "I'm in the index action!"
+        render User.all 
+    end
+
+    def show
+        # user = User.find_by(id: params[:id])
+
+        render User 
+    end
+
+    def update
+        user = User.
+    end
+
+    def destroy
+    end
+
+    def create
+        render json: params
+    end
+
+#     {
+#   "fav_food": "pizza",
+#   "controller": "users",
+#   "action": "create"
+# }
+
+    def edit
+    end
 end
